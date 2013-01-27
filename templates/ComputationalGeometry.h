f38f1367fcaa608172af86b23d0f456fa32b38d8
boleynsu <boleyn.su@gmail.com>
2013-01-27 20:39:30 +0800

    更新计算几何模板 添加求凸包的算法 添加求两线段交点的函数 p.s.上一次commit是添加SGU129不是132

--- templates/ComputationalGeometry.h
+++ templates/ComputationalGeometry.h
@@ -24,26 +24,31 @@ using ::sgn;
 using ::dbcmp;
 //点积
 using ::dot;
+inline
 Number dot(const Point& a,const Point& b,const Point& c)
 {
 	rtn dot(b-a,c-a);
 }
 //叉积
 using ::cross;
+inline
 Number cross(const Point& a,const Point& b,const Point& c)
 {
 	rtn cross(b-a,c-a);
 }
 //长度
+inline
 Number len(const Vector& v)
 {
 	rtn sqrt(dot(v,v));
 }
+inline
 Number len(const Segment& s)
 {
 	rtn len(s.x-s.y);
 }
 //距离
+inline
 Number dis(const Point& a,const Point& b)
 {
 	rtn len(b-a);
@@ -54,7 +59,7 @@ Number dis(const Point& a,const Point& b)
 int point_on_segment(const Point& p,const Segment& s)
 {
 	if (sgn(cross(p,s.x,s.y))) rtn 0;
-	else rtn dbcmp(dis(s.x,p)+dis(s.y,p),len(s))==0?1:-1;
+	else rtn sgn(dot(p,s.x,s.y))<=0?1:-1;
 }
 
 //Ray Casting Algorithm 射线法判断点是否在简单多边形内
@@ -68,6 +73,34 @@ int point_in_polygon(const Point& p,const vec<Segment>& e)
 			&&dbcmp(p.x,(e[i].y.x-e[i].x.x)/(e[i].y.y-e[i].x.y)*(p.y-e[i].x.y)+e[i].x.x)<0)
 			in=!in;
 	return in?1:-1;
+}
+
+//求线段交点
+//如果平行则返回(+inf,+inf) 否则返回交点 交点为线段所在直线的交点
+Point intersection(const Segment a,const Segment& b)
+{
+	Vector va=a.y-a.x,vb=b.y-b.x;
+	if (!sgn(cross(va,vb))) rtn Point(+inf,+inf);
+	else rtn a.x+va*(cross(b.x-a.x,vb)/cross(va,vb));
+}
+
+//Andrew's Monotone Chain算法  求凸包
+void get_convex_hull(Polygon& PO)
+{
+	srt(PO);
+	Polygon CH;
+    rep(i,sz(PO))
+    {
+        while (sz(CH)>=2&&sgn(cross(CH[sz(CH)-2],CH[sz(CH)-1],PO[i]))<=0) CH.pop_back();
+        CH.pb(PO[i]);
+    }
+    for (int i=sz(PO)-1,t=sz(CH)+1;i>=0;--i)
+    {
+        while (sz(CH)>=t&&sgn(cross(CH[sz(CH)-2],CH[sz(CH)-1],PO[i]))<=0) CH.pop_back();
+        CH.pb(PO[i]);
+    }
+    CH.pop_back();
+    PO.swap(CH);
 }
 
 }
