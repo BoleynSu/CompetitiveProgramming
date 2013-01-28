49463d9542a2562889c6cd76555075f4a3ad22ac
boleynsu <boleyn.su@gmail.com>
2013-01-28 23:14:00 +0800

    delete bin

---
+++ java-workspace/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String arg[])
+	{
+		new Main();
+	}
+	public Main()
+	{
+		Scanner cin=new Scanner(new BufferedInputStream(System.in));
+		cin.close();
+	}
+}
