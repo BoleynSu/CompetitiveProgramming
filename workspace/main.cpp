0e4e77c16d1fb91e8c63a5ee36c7c66c953c541e
boleynsu <boleyn.su@gmail.com>
2013-02-03 04:40:21 +0800

    Hacker Cup

--- workspace/main.cpp
+++ workspace/main.cpp
@@ -127,8 +127,8 @@ lli ooll=(~0ull)>>1;
 db inf=1e+10;
 db eps=1e-10;
 db pi=acos(-1.0);
-//int dx[]={-1,1,0,0,-1,-1,1,1,0};
-//int dy[]={0,0,-1,1,-1,1,-1,1,0};
+int dx[]={-1,1,0,0,-1,-1,1,1,0};
+int dy[]={0,0,-1,1,-1,1,-1,1,0};
 int MOD=1000000007;
 
 template<typename type>inline bool cmax(type& a,const type& b){rtn a<b?a=b,true:false;}
@@ -162,163 +162,281 @@ template<typename type>inline void merge(type& a,type& b){if(sz(a)<sz(b))swap(a,
 struct Initializer{Initializer(){ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);}~Initializer(){runtime();}}initializer;
 //end #include <Core>
 
-const int w=100,l=100,h=20;
-const db a=45.0/(460.0*7860.0);
-const db dx=0.2/w,dy=0.2/l,dz=0.01/h,dT=1.0;
-const int times=200;
-db T[2][w][l][h];
+/*
+ * Package: StandardCodeLibrary.NumberTheory
+ * Description:
+ * O(n)的筛法求素数表;
+ * 带mod的乘法;
+ * 带mod的指数函数;
+ * Rabin-Miller素数测试;
+ * Pollard's rho大数分解;
+ * 计算phi;
+ * 扩展gcd;
+ * 中国剩余定理;
+ * 指数循环节;
+ * */
+//#include <Core>
 
-void circle()
+namespace StandardCodeLibrary
 {
-	rep(x,w) rep(y,l) rep(z,h)
-		if (sqr(x-w/2)+sqr(y-l/2)<sqr(w/2))
-		{
-			if (z==0) T[0&1][x][y][z]=273+100;
-			else if (z==h-1) T[0&1][x][y][z]=273+190;
-			else T[0&1][x][y][z]=273+100;
-		}
-		else T[0&1][x][y][z]=273+190;
-	db second=0;
-	ft(i,1,times)
+namespace Math
+{
+namespace NumberTheory
+{
+
+//O(n)的筛法求素数表
+//MAXPS=[1,MAXP]中素数的个数
+const int MAXP=10000000;
+const int MAXPS=664579;
+bool isp[MAXP+1];//isp[x]=x是否为素数
+int pp[MAXP+1];//pp[x]=x在素数表中的位置(从0开始)
+int fac[MAXP+1];//fax[x]=x最小的质因子(x<=1时无意义)
+int ps;//素数表的大小
+int p[MAXPS];//素数表
+void make_prime_table()
+{
+	fl(isp,true);
+	isp[0]=isp[1]=false;
+	ft(i,2,MAXP)
 	{
-		db dt=inf;
-		rep(x,w) rep(y,l) rep(z,h)
-			if (sqr(x-w/2)+sqr(y-l/2)<sqr(w/2-1))
-				if (z!=0&&z!=h-1)
-				{
-					db delta=dT/abs(a*(
-							(T[(i-1)&1][x+1][y][z]-2*T[(i-1)&1][x][y][z]+T[(i-1)&1][x-1][y][z])/sqr(dx)
-							+(T[(i-1)&1][x][y+1][z]-2*T[(i-1)&1][x][y][z]+T[(i-1)&1][x][y-1][z])/sqr(dy)
-							+(T[(i-1)&1][x][y][z+1]-2*T[(i-1)&1][x][y][z]+T[(i-1)&1][x][y][z-1])/sqr(dz)));
-					cmin(dt,delta);
-				}
-		second+=dt;
-		rep(x,w) rep(y,l) rep(z,h)
-			if (sqr(x-w/2)+sqr(y-l/2)<sqr(w/2-1))
-			{
-				if (z==0) T[i&1][x][y][z]=273+100;
-				else if (z==h-1) T[i&1][x][y][z]=273+190;
-				else
-				{
-					T[i&1][x][y][z]=T[(i-1)&1][x][y][z]+dt*a*(
-							(T[(i-1)&1][x+1][y][z]-2*T[(i-1)&1][x][y][z]+T[(i-1)&1][x-1][y][z])/sqr(dx)
-							+(T[(i-1)&1][x][y+1][z]-2*T[(i-1)&1][x][y][z]+T[(i-1)&1][x][y-1][z])/sqr(dy)
-							+(T[(i-1)&1][x][y][z+1]-2*T[(i-1)&1][x][y][z]+T[(i-1)&1][x][y][z-1])/sqr(dz));
-				}
-			}
-			else T[i&1][x][y][z]=273+190;
-		cout<<"i="<<i<<endl;
-		cout<<"t=",pdb(3,second)<<" dt=",pdb(10,dt)<<" T[O]="<<T[i&1][w/2][h/2][h/2]<<endl;
-		if (i==times)
+		if (isp[i]) pp[p[ps]=i]=ps,ps++,fac[i]=i;
+		for (int j=0;p[j]*i<=MAXP;j++)
 		{
-			//ListDensityPlot[{}, InterpolationOrder -> 3, ColorFunction -> "TemperatureMap", PerformanceGoal -> "Quality"]
-			ofstream cout("E:\\out.txt");
-			rep(x,w) rep(y,l) if ((sqr(x-w/2)+sqr(y-l/2)<sqr(w/2-1)))
-				cout<<"{"<<x<<","<<y<<",",pdb(5,T[i&1][x][y][1]-273)<<"},";
-			cout<<endl;
-			rep(x,w) rep(y,l) if ((sqr(x-w/2)+sqr(y-l/2)<sqr(w/2-1)))
-			{
-				db ans=0;
-				rep(z,h) ans+=T[i&1][x][y][z];
-				ans/=h;
-				cout<<"{"<<x<<","<<y<<",",pdb(5,ans-273)<<"},";
-			}
-			cout<<endl;
-		}
-		if (i%100==99)
-		{
-			cout<<"quit?"<<endl;
-			char c;
-			if (cin>>c,c=='q')
-			{
-				//ListContourPlot[{}, InterpolationOrder -> 3, ColorFunction -> "TemperatureMap", PerformanceGoal -> "Quality"]
-				ofstream cout("E:\\out.txt");
-				//rep(x,w) rep(y,l) if ((sqr(x-w/2)+sqr(y-l/2)<sqr(w/2-1)))
-				//	cout<<"{"<<x<<","<<y<<",",pdb(5,T[i&1][x][y][1]-273)<<"},";
-				//cout<<endl;
-				cout<<"ListContourPlot[{";
-				rep(x,w) rep(y,l) if ((sqr(x-w/2)+sqr(y-l/2)<sqr(w/2-1)))
-				{
-					db ans=0;
-					rep(z,h) ans+=T[i&1][x][y][z];
-					ans/=h;
-					cout<<"{"<<x<<","<<y<<",",pdb(5,ans-273)<<"},";
-				}
-				cout<<"}, InterpolationOrder -> 3, ColorFunction -> \"TemperatureMap\", PerformanceGoal -> \"Quality\"]"<<endl;
-			}
+			isp[p[j]*i]=false,fac[p[j]*i]=p[j];
+			if(i%p[j]==0) break;
 		}
 	}
 }
 
-void rectangular()
+//带mod的乘法
+lli mulWithMod(lli x,lli y,lli z)
 {
-	rep(x,w) rep(y,l) rep(z,h)
-		if (x!=0&&x!=w-1&&y!=0&&y!=l-1&&z!=h-1) T[0&1][x][y][z]=273+100;
-		else T[0&1][x][y][z]=273+190;
-
-	db second=0;
-	int i;
-
-	for (i=1;;i++)
+	lli ret=0;
+	x%=z;
+	y%=z;
+	whl(y)
 	{
-		db dt=inf;
-		rep(x,w) rep(y,l) rep(z,h)
-			if (x!=0&&x!=w-1&&y!=0&&y!=l-1&&z!=0&&z!=h-1)
-			{
-				db delta=dT/abs(a*(
-						(T[(i-1)&1][x+1][y][z]-2*T[(i-1)&1][x][y][z]+T[(i-1)&1][x-1][y][z])/sqr(dx)
-						+(T[(i-1)&1][x][y+1][z]-2*T[(i-1)&1][x][y][z]+T[(i-1)&1][x][y-1][z])/sqr(dy)
-						+(T[(i-1)&1][x][y][z+1]-2*T[(i-1)&1][x][y][z]+T[(i-1)&1][x][y][z-1])/sqr(dz)));
-				cmin(dt,delta);
-			}
-		second+=dt;
-
-		rep(x,w) rep(y,l) rep(z,h)
-			if (x!=0&&x!=w-1&&y!=0&&y!=l-1&&z!=h-1)
-			{
-				if (z==0) T[i&1][x][y][z]=273+100;
-				else
-				{
-					T[i&1][x][y][z]=T[(i-1)&1][x][y][z]+dt*a*(
-							(T[(i-1)&1][x+1][y][z]-2*T[(i-1)&1][x][y][z]+T[(i-1)&1][x-1][y][z])/sqr(dx)
-							+(T[(i-1)&1][x][y+1][z]-2*T[(i-1)&1][x][y][z]+T[(i-1)&1][x][y-1][z])/sqr(dy)
-							+(T[(i-1)&1][x][y][z+1]-2*T[(i-1)&1][x][y][z]+T[(i-1)&1][x][y][z-1])/sqr(dz));
-				}
-			}
-			else T[i&1][x][y][z]=273+190;
-
-		cout<<"i="<<i<<endl;
-		db TC=0,TO=0;
-		for (int x:{1,w-2}) for (int y:{1,l-2}) rep(z,h) TC+=T[i&1][x][y][z];
-		TC/=4*h;
-		rep(z,h) TO+=T[i&1][w/2][l/2][z];
-		TO/=h;
-		cout<<"t=",pdb(3,second)<<" dt=",pdb(10,dt)<<" T[O]="<<TO<<" T[C]="<<TC<<endl;
-		/*
-		if (i%100==99)
+		if (y&1)
 		{
-			cout<<"quit?"<<endl;
-			char c;
-			if (cin>>c,c=='q') break;
+			ret+=x;
+			if (ret>=z) ret-=z;
 		}
-		*/
-		if (i==times) break;
+		x<<=1;
+		if (x>=z) x-=z;
+		y>>=1;
 	}
-
-	ofstream cout("D:\\out.m");
-	cout<<"ListContourPlot[{";
-	rep(x,w) rep(y,l) if (x!=0&&x!=w-1&&y!=0&&y!=l-1)
-	{
-		db ans=0;
-		rep(z,h) ans+=T[i&1][x][y][z];
-		ans/=h;
-		cout<<"{"<<x<<","<<y<<",",pdb(5,ans-273)<<"}";
-		if (x!=w-2||y!=l-2) cout<<",";
-	}
-	cout<<"}, InterpolationOrder -> 3, ColorFunction -> \"TemperatureMap\", PerformanceGoal -> \"Quality\"]"<<endl;
+	rtn ret;
 }
+
+//带mod的指数函数
+lli powWithMod(lli x,lli y,lli z)
+{
+	lli ret=1;
+	x%=z;
+	whl(y)
+	{
+		if (y&1) ret=mulWithMod(ret,x,z);
+		y>>=1;
+		x=mulWithMod(x,x,z);
+	}
+	rtn ret;
+}
+
+//Rabin-Miller素数测试
+bool isProbablePrime(lli n,lli k=50)
+{
+	if (n<=1) rtn false;
+	else if (n<=3) rtn true;
+	else
+	{
+		lli d=n-1;
+		whl(!(d&1)) d>>=1;
+		rep(i,k)
+		{
+			lli a=rand()%(n-3)+2;//2 to n-2
+			lli x=powWithMod(a,d,n);
+			if (x==1) continue;
+			whl(d!=n-1&&x!=n-1&&x!=1)
+			{
+				x=mulWithMod(x,x,n);
+				d<<=1;
+			}
+			if (x!=n-1) rtn false;
+		}
+		rtn true;
+	}
+}
+
+//Pollard's rho大数分解
+lli factor(lli n,lli k=50)
+{
+	if (isProbablePrime(n,k)) rtn n;
+	lp
+	{
+		lli d=1,x=rand()%(n-1)+1,//1 to n-1
+				y=rand()%(n-1)+1,//1 to n-1
+				c=rand()%n;
+		if (c==0) c++;//c!=0
+		if (c==n-2) c++;//c!=n-2
+		lli loop=0;
+		whl(d==1)
+		{
+			loop++;
+			if (((-loop)&loop)==loop) y=x;
+			x=(mulWithMod(x,x,n)+c)%n;
+			if (x==y) break;
+			d=gcd(y-x+n,n);
+			if (d!=1&&d!=n) rtn factor(d,k);
+		}
+	}
+}
+
+//计算phi
+lli phi(lli x,lli k=50)
+{
+	lli ret=x;
+	whl(x!=1)
+	{
+		lli d=factor(x,k);//如果是小范围的 此处可用fac[x]代替
+		ret/=d;
+		ret*=d-1;
+		whl(x%d==0) x/=d;
+	}
+	rtn ret;
+}
+
+//扩展gcd
+lli gcd(lli a,lli b,lli& x,lli& y)
+{
+	if (b)
+	{
+		lli g=gcd(b,a%b,y,x);
+		rtn y-=a/b*x,g;
+	}
+	else rtn x=1,y=0,a;
+}
+
+//中国剩余定理
+//一般地,中国剩余定理是指若有一些两两互质的整数m[i],则对任意的整数a[i],以下联立同余方程组对模m[i]有公解
+//x mod m[i]=a[i]
+lli chinese_remainder(lli n,lli m[],lli a[])
+{
+	lli lcm=1;
+	rep(i,n) lcm*=m[i];
+	lli ans=0;
+	rep(i,n)
+	{
+		lli Mi=lcm/m[i],x,y;
+		gcd(Mi,m[i],x,y);
+		ans+=Mi*x*a[i];
+		ans%=lcm;
+	}
+	if (ans<0) ans+=lcm;
+	rtn ans;
+}
+
+//指数循环节
+//如果(a,p)=1		a^x%p=a^(x%phi(p))%p
+//否则若x>=phi(p)	a^x%p=a^(x%phi(p)+phi(p))%p
+lli mod(lli x,lli phip)
+{
+	rtn x>=phip?x%phip+phip:x;
+}
+
+}
+}
+}
+
+using namespace StandardCodeLibrary::Math::NumberTheory;
+
+lli inv(lli x)
+{
+	lli y,ans;
+	gcd(x,MOD,ans,y);
+	rtn ans;
+}
+/*A
+int get()
+{
+	int n,k;
+	cin>>n>>k;
+	vec<lli> a(n),f(n);
+	rep(i,n) cin>>a[i];
+	srt(a);
+	f[0]=1;
+	repf(i,1,n) f[i]=f[i-1]*i%MOD;
+	lli ans=0;
+	repf(i,k-1,n) ans=(ans+f[i]*inv(f[k-1])%MOD*inv(f[i-(k-1)])%MOD*a[i])%MOD;
+	if (ans<0) ans+=MOD;
+	rtn ans;
+}
+*/
+/*PB
+str match(str k1,str k2)
+{
+	str ans;
+	ans.resize(sz(k1));
+
+	mci cnt;
+	rep(i,sz(k1)) cnt[k2[i]]++;
+	rep(i,sz(k1)) if (k1[i]!='?')
+	{
+		if (cnt[k1[i]]) cnt[k1[i]]--;
+		else if (cnt['?']) cnt['?']--;
+		else rtn "";
+		ans[i]=k1[i];
+	}
+	cnt['a']+=cnt['?'];
+	cnt['?']=0;
+	rep(i,sz(k1)) if (k1[i]=='?')
+		ft(k,'a','f')
+		if (cnt[k])
+		{
+			ans[i]=k,cnt[k]--;
+			break;
+		}
+	rtn ans;
+}
+bool aug(vvi& adj,vb& vis,vi& mat,int u)
+{
+    vis[u]=true;
+    rep(i,sz(adj[u]))
+        if (mat[adj[u][i]]==-1||(!vis[mat[adj[u][i]]]&&aug(adj,vis,mat,mat[adj[u][i]])))
+        {
+            mat[adj[u][i]]=u;
+            mat[u]=adj[u][i];
+            return true;
+        }
+    return false;
+}
+
+str get()
+{
+	int m;
+	str k1,k2;
+	cin>>m>>k1>>k2;
+	vvi adj(m*2);
+	vi mat(m*2,-1);
+	rep(i,m)
+		rep(j,m)
+			if (sz(match(k1.substr(i*sz(k1)/m,sz(k1)/m),k2.substr(j*sz(k1)/m,sz(k1)/m))))
+				adj[i].pb(m+j),adj[j+m].pb(i);
+	str ans;
+	rep(i,m)
+	{
+		vb vis(sz(k1)/m*2);
+		if (mat[i]==-1&&!aug(adj,vis,mat,i)) rtn "IMPOSSIBLE";
+	}
+	rep(i,m) ans=ans+match(k1.substr(i*sz(k1)/m,sz(k1)/m),k2.substr((mat[i]-m)*sz(k1)/m,sz(k1)/m));
+	rtn ans;
+}
+*/
 
 int main()
 {
-	rectangular();
+	freopen("in.txt","r",stdin);
+	freopen("out.txt","w",stdout);
+	int t;
+	cin>>t;
+	ft(i,1,t) cout<<"Case #"<<i<<": "<<get()<<endl;
 }
