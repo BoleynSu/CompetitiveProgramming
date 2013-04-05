b400f91057cb9e78dc8ebbbaf5e0aface64bbfba
boleynsu <boleyn.su@gmail.com>
2013-04-05 17:57:21 +0800

    更新计算几何模板

--- templates/ComputationalGeometry.h
+++ templates/ComputationalGeometry.h
@@ -58,34 +58,78 @@ Number dis(const Point& a,const Point& b)
 	rtn len(b-a);
 }
 
+//判断点是否在直线上
+//如果点在直线上返回1 不在直线上返回0
+int point_on_line(const Point& p,const Line& l)
+{
+	if (sgn(cross(p,l.x,l.y))) rtn 0;
+	else rtn 1;
+}
 //判断点是否在线段上
 //如果点在线段上返回1 不在线段上但在直线上 返回-1 不在直线上返回0
 int point_on_segment(const Point& p,const Segment& s)
 {
-	if (sgn(cross(p,s.x,s.y))) rtn 0;
-	else rtn sgn(dot(p,s.x,s.y))<=0?1:-1;
+	if (point_on_line(p,s)) rtn sgn(dot(p,s.x,s.y))<=0?1:-1;
+	else rtn 0;
 }
 
 //Ray Casting Algorithm 射线法判断点是否在简单多边形内
-//在内部返回1 在外部返回-1 在边上返回0
+//在内部返回1 在外部返回0 在边上返回-1
 int point_in_polygon(const Point& p,const vec<Segment>& e)
 {
-	rep(i,sz(e)) if (point_on_segment(p,e[i])==1) rtn 0;
+	rep(i,sz(e)) if (point_on_segment(p,e[i])==1) rtn -1;
 	bool in=false;
 	rep(i,sz(e))
 		if ((dbcmp(e[i].x.y,p.y)>0)!=(dbcmp(e[i].y.y,p.y)>0)
 			&&dbcmp(p.x,(e[i].y.x-e[i].x.x)/(e[i].y.y-e[i].x.y)*(p.y-e[i].x.y)+e[i].x.x)<0)
 			in=!in;
-	return in?1:-1;
+	return in?1:0;
 }
 
-//求线段交点
-//如果平行则返回(+inf,+inf) 否则返回交点 交点为线段所在直线的交点
-Point intersection(const Line& a,const Line& b)
+//判断直线是否相交
+//1表示相交 -1表示重合 0表示平行
+int line_intersect(const Line& a,const Line& b)
 {
 	Vector va=a.y-a.x,vb=b.y-b.x;
-	if (!sgn(cross(va,vb))) rtn Point(+inf,+inf);
-	else rtn a.x+va*(cross(b.x-a.x,vb)/cross(va,vb));
+	if (sgn(cross(va,vb))) rtn 1;
+	else
+	{
+		if (sgn(cross(a.x,a.y,b.x))) rtn 0;
+		else rtn -1;
+	}
+}
+
+//判断线段是否相交
+//1表示有一个交点
+//-1表示有无穷多个交点(两线段平行但实际只有一个交点也算入了这种情况)
+//0表示不相交
+int segment_intersect(const Segment& a,const Segment& b)
+{
+	int ab=sgn(cross(a.x,a.y,b.x))*sgn(cross(a.x,a.y,b.y)),
+		ba=sgn(cross(b.x,b.y,a.x))*sgn(cross(b.x,b.y,a.y));
+	if (ab>0||ba>0) rtn 0;
+	else
+	{
+		Vector va=a.y-a.x,vb=b.y-b.x;
+		if (sgn(cross(va,vb))) rtn 1;
+		else
+		{
+			if (sgn(dot(b.x,a.x,a.y))<=0
+				||sgn(dot(b.y,a.x,a.y))<=0
+				||sgn(dot(a.x,b.x,b.y))<=0
+				||sgn(dot(a.y,b.x,b.y))<=0)
+				rtn -1;
+			else rtn 0;
+		}
+	}
+}
+
+//求直线交点
+//需保证直线不平行 返回直线交点
+Point line_intersection(const Line& a,const Line& b)
+{
+	Vector va=a.y-a.x,vb=b.y-b.x;
+	rtn a.x+va*(cross(b.x-a.x,vb)/cross(va,vb));
 }
 
 //Andrew's Monotone Chain算法  求凸包
@@ -122,7 +166,7 @@ bool halfplane_intersection_judge(const Halfplane& a,const Halfplane& b,const Ha
 	{
 		if (sgn(cross(a.y-a.x,b.y-b.x)))
 		{
-			Point ab=intersection(a,b);
+			Point ab=line_intersection(a,b);
 			return sgn(cross(c.x,c.y,ab))<=0;//如果交集可以退化成线或者点 <=变<
 		}
 		else return false;
@@ -159,7 +203,7 @@ bool halfplane_intersection(Polygon& CH,vec<Halfplane>& AHP,vec<Halfplane> HP)
 		CH.resize(sz(q)),AHP.resize(sz(q));
 		rep(i,sz(q)) AHP[i]=q[i];
 		for (int i=0,j=sz(AHP)-1;i<sz(AHP);j=i++)
-			CH[i]=intersection(AHP[i],AHP[j]);
+			CH[i]=line_intersection(AHP[i],AHP[j]);
 		return true;
 	}
 }
