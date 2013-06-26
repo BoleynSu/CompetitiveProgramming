f17c4b69cba84595a7ec4766f9662e9296133d45
boleynsu <boleyn.su@gmail.com>
2013-06-27 02:42:13 +0800

    更新Core 添加mod函数 去除feach宏 添加注释 改变顺序

--- haskell-workspace/src/Main.hs
+++ haskell-workspace/src/Main.hs
@@ -1,2 +1,10 @@
 
-main = putStrLn "hello world!"
+main =
+  do
+    input <- getContents
+    let
+      (x:y:m:_) = map (read::String->Int) (words input)
+    putStr $ show $ get x y m
+  where
+    get x y m | max x y >= m = 0
+              | otherwise    = 1
