1325c2593dc93860802df4174e6f4dd1e8cc8aed
boleynsu <boleyn.su@gmail.com>
2013-07-03 03:02:13 +0800

    do nothing

--- cpp-workspace/src/main.cpp
+++ cpp-workspace/src/main.cpp
@@ -1,10 +1,15 @@
-#include <Math>
+#include <Core>
 
-db r1,r2;
-db f(db x){rtn x;}
 int main()
 {
-	cin>>r1>>r2;
-	if (r1>r2) swap(r1,r2);
-	pdb(4,integrate(0,r1,f))<<endl;
+	int n;
+	cin>>n;
+	vi a(n);
+	cin>>a;
+	vi xs,ys;
+	xs.clear(),ys.clear();
+	rep(i,n)
+	{
+
+	}
 }
