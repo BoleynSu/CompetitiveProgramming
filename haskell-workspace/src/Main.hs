e9221b56989c94b55f8bda6d4f0658593a7b5911
boleynsu <boleyn.su@gmail.com>
2013-07-14 00:59:03 +0800

    before MemSQL start[c]up Round 1

--- haskell-workspace/src/Main.hs
+++ haskell-workspace/src/Main.hs
@@ -12,40 +12,31 @@ build l r = Node left right l r 0
         right = build (m+1) r
         m     = (l+r) `div` 2
 
-update::SegmentTree->Int->Int->Long->SegmentTree
-update Null _ _ _ = Null
-update (Node left right l r value) begin end _ | r < begin || end < l = Node left right l r value
-update (Node left right l r _) begin end x | begin <= l && r<=end = Node newLeft newRight l r x
-  where newLeft  = update left begin end x
-        newRight = update right begin end x
-update (Node left right l r _) begin end x = Node newLeft newRight l r newValue
-  where newLeft  @ (Node _ _ _ _ newLeftValue)  = update left begin end x
-        newRight @ (Node _ _ _ _ newRightValue) = update right begin end x
-        newValue = max newLeftValue newRightValue
+update::Int->Int->Long->SegmentTree->SegmentTree
+update begin end x = update'
+  where update' Null =Null
+        update' t @ (Node _ _ l r _) | r < begin || end < l = t
+        update' (Node left right l r _) | begin <= l && r<=end = let
+          newLeft  = update' left
+          newRight = update' right
+          in Node newLeft newRight l r x
+        update' (Node left right l r _) = let 
+          newLeft  @ (Node _ _ _ _ newLeftValue)  = update' left
+          newRight @ (Node _ _ _ _ newRightValue) = update' right
+          newValue = max newLeftValue newRightValue
+          in Node newLeft newRight l r newValue
 
-query::SegmentTree->Int->Int->Long
-query Null _ _ = 0
-query (Node _ _ l r _) begin end | r < begin || end < l = 0 
-query (Node _ _ l r value) begin end | begin<=l && r<=end = value 
-query (Node left right _ _ _) begin end = max (query left begin end) (query right begin end)
+query::Int->Int->SegmentTree->Long
+query begin end = query'
+  where query' Null = 0
+        query' (Node _ _ l r _) | r < begin || end < l = 0
+        query' (Node _ _ l r value) | begin <= l && r <= end = value 
+        query' (Node left right _ _ _) = max (query' left) (query' right)
 
 makeTuple::[Int]->[(Int,Int)]
 makeTuple [ ] = []
 makeTuple [_] = []
 makeTuple (a:b:c) = (a,b) : makeTuple c
-
-initTree::SegmentTree->Int->[Int]->SegmentTree
-initTree t _ [] = t
-initTree t p (x:xs) = initTree (update t p p (fromIntegral x::Long)) (p+1) xs
-
-solveAQuery::SegmentTree->[(Int,Int)]->[Long]
-solveAQuery _ [] = []
-solveAQuery t ((w,h):xs) = ans:solveAQuery newT xs
-  where ans  = query t 1 w
-        newT = update t 1 w (ans+(fromIntegral h::Long))
-
-solve::Int->[Int]->[(Int,Int)]->[Long]
-solve n a = solveAQuery (initTree (build 1 n) 1 a)
 
 main::IO()
 main = do
@@ -55,4 +46,16 @@ main = do
     a = take n number0
     (m:number1) = drop n number0
     wh = take m (makeTuple number1)
-  putStr $ foldr (\x y->show x ++ "\n" ++ y) "" (solve n a wh)
+    t = initTree 1 a $ build 1 n
+  solve t wh
+  where initTree::Int->[Int]->SegmentTree->SegmentTree
+        initTree _ [] t = t
+        initTree p (x:xs) t = initTree (p+1) xs $ update p p (fromIntegral x::Long) t
+
+        solve::SegmentTree->[(Int,Int)]->IO()
+        solve _ [] = return ()
+        solve t ((w,h):xs) = do
+          print ans
+          solve nt xs
+          where ans = query 1 w t
+                nt  = update 1 w (ans+(fromIntegral h::Long)) t
