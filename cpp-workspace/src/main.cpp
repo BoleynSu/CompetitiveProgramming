3d096ee745c796e9d0c02c485d37ada9db68018f
boleynsu <boleyn.su@gmail.com>
2013-10-04 19:47:44 +0800

    update

--- cpp-workspace/src/main.cpp
+++ cpp-workspace/src/main.cpp
@@ -1,198 +1,180 @@
-#ifndef DEBUG
-//https://github.com/boleynsu/acmicpc-codes/blob/master/templates/Core.hpp
-/*
- * Package: StandardCodeLibrary.Core
- * */
-//引进常用的头文件并使用std名字空间;
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <iomanip>
-#include <utility>
-#include <vector>
-#include <list>
-#include <string>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <set>
-#include <map>
-#include <algorithm>
-#include <functional>
-#include <numeric>
-#include <bitset>
-#include <complex>
-#include <cstdio>
 #include <cstring>
-#include <cmath>
-#include <cstdlib>
-#include <ctime>
-#include <climits>
+#include <cstdio>
+#include <algorithm>
+#include <map>
+#include <utility>
 using namespace std;
 
-//用于减少代码量的宏;
-#define lp for(;;)
-#define repf(i,a,b) for (int i=(a);i<(b);++i)
-#define ft(i,a,b) for (int i=(a);i<=(b);++i)
-#define fdt(i,a,b) for (int i=(a);i>=(b);--i)
-#define rrepf(i,a,b) fdt(i,(a)-1,b)
-#define rep(i,n) repf(i,0,n)
-#define rrep(i,n) rrepf(i,n,0)
-#define for_nonempty_subsets(subset,set) for (int subset=set;subset;subset=(subset-1)&(set))
-#define for_in_charset(i,charset) for (cstr i=(charset);*i;i++)
-#define whl while
-#define rtn return
-#define fl(x,y) memset((x),char(y),sizeof(x))
-#define clr(x) fl(x,char(0))
-#define cpy(x,y) memcpy(x,y,sizeof(x))
-#define sf scanf
-#define pf printf
-#define vec vector
-#define pr pair
-#define que queue
-#define prq priority_queue
-#define itr iterator
-#define x first
-#define y second
-#define pb push_back
-#define mp make_pair
-#define ins insert
-#define ers erase
-#define lb lower_bound
-#define ub upper_bound
-#define rnk order_of_key
-#define sel find_by_order
-#define sz(x) (int((x).size()))
-#define all(x) (x).begin(),(x).end()
-#define srt(x) sort(all(x))
-#define uniq(x) srt(x),(x).erase(unique(all(x)),(x).end())
-#define rev(x) reverse(all(x))
-#define shf(x) random_shuffle(all(x))
-#define nxtp(x) next_permutation(all(x))
-
-//调试相关的宏;
-#ifndef DEBUG
-#define prt(x) (cerr)
-#define asrtWA(s) do if(!(s))exit(0);whl(0)
-#define asrtTLE(s) do if(!(s))whl(1);whl(0)
-#define asrtMLE(s) do if(!(s))whl(new int);whl(0)
-#define asrtOLE(s) do if(!(s))whl(1)puts("OLE");whl(0)
-#define asrtRE(s) do if(!(s))*(int*)0=0;whl(0)
-#define runtime() (cerr)
-#define input(in) freopen(in,"r",stdin)
-#define output(out) freopen(out,"w",stdout)
-#else
-#define prt(x) cerr<<"第"<<__LINE__<<"行\t: "<<#x"\t="<<(x)<<endl
-#define asrtWA(s) do if(!(s))cerr<<"assert("#s")"<<endl;whl(0)
-#define asrtTLE(s) do if(!(s))cerr<<"assert("#s")"<<endl;whl(0)
-#define asrtMLE(s) do if(!(s))cerr<<"assert("#s")"<<endl;whl(0)
-#define asrtOLE(s) do if(!(s))cerr<<"assert("#s")"<<endl;whl(0)
-#define asrtRE(s) do if(!(s))cerr<<"assert("#s")"<<endl;whl(0)
-#define runtime() cerr<<"Used: "<<db(clock())/CLOCKS_PER_SEC<<"s"<<endl
-#define input(in)
-#define output(out)
-#endif
-
-//常用数据类型;
-typedef long long int lli;
-typedef double db;
-typedef const char* cstr;
-typedef string str;
-typedef vec<int> vi;
-typedef vec<vi> vvi;
-typedef vec<lli> vl;
-typedef vec<vl> vvl;
-typedef vec<bool> vb;
-typedef vec<vb> vvb;
-typedef vec<char> vc;
-typedef vec<vc> vvc;
-typedef vec<str> vs;
-typedef pr<int,int> pii;
-typedef pr<lli,lli> pll;
-typedef pr<db,db> pdd;
-typedef vec<pii> vpii;
-typedef vec<pll> vpll;
-typedef vec<pdd> vpdd;
-typedef map<int,int> mii;
-typedef map<str,int> msi;
-typedef map<char,int> mci;
-typedef set<int> si;
-typedef set<str> ss;
-typedef que<int> qi;
-
-//常用常量:int的最大值;lli的最大值;db的误差相关常数;欧拉常数;圆周率;移动向量;取模使用的除数;
-int oo=(~0u)>>1;
-lli ooll=(~0ull)>>1;
-db inf=1e+10;
-db eps=1e-10;
-db gam=0.5772156649015328606;
-db pi=acos(-1.0);
-int dx[]={1,0,-1,0,1,-1,-1,1,0};
-int dy[]={0,1,0,-1,1,1,-1,-1,0};
-int MOD=1000000007;
-
-//常用函数:最大最小值更新;数学相关函数;输入和输出;树状数组;并查集;可合并堆;
-template<typename type>inline bool cmax(type& a,const type& b){rtn a<b?a=b,true:false;}
-template<typename type>inline bool cmin(type& a,const type& b){rtn b<a?a=b,true:false;}
-template<typename type>inline type sqr(const type& x){rtn x*x;}
-template<typename type>inline type mod(const type& x){rtn x%MOD;}
-inline int sgn(const db& x){rtn (x>+eps)-(x<-eps);}
-inline int dbcmp(const db& a,const db& b){rtn sgn(a-b);}
-template<typename type>inline pr<type,type> operator-(const pr<type,type>& x){rtn mp(-x.x,-x.y);}
-template<typename type>inline pr<type,type> operator+(const pr<type,type>& a,const pr<type,type>& b){rtn mp(a.x+b.x,a.y+b.y);}
-template<typename type>inline pr<type,type> operator-(const pr<type,type>& a,const pr<type,type>& b){rtn mp(a.x-b.x,a.y-b.y);}
-template<typename type>inline pr<type,type> operator*(const pr<type,type>& a,const type& b){rtn mp(a.x*b,a.y*b);}
-template<typename type>inline pr<type,type> operator/(const pr<type,type>& a,const type& b){rtn mp(a.x/b,a.y/b);}
-template<typename type>inline pr<type,type>& operator-=(pr<type,type>& a,const pr<type,type>& b){rtn a=a-b;}
-template<typename type>inline pr<type,type>& operator+=(pr<type,type>& a,const pr<type,type>& b){rtn a=a+b;}
-template<typename type>inline pr<type,type>& operator*=(pr<type,type>& a,const type& b){rtn a=a*b;}
-template<typename type>inline pr<type,type>& operator/=(pr<type,type>& a,const type& b){rtn a=a/b;}
-template<typename type>inline type cross(const pr<type,type>& a,const pr<type,type>& b){rtn a.x*b.y-a.y*b.x;}
-template<typename type>inline type dot(const pr<type,type>& a,const pr<type,type>& b){rtn a.x*b.x+a.y*b.y;}
-template<typename type>inline type gcd(type a,type b){if(b)whl((a%=b)&&(b%=a));rtn a+b;}
-template<typename type>inline type lcm(type a,type b){rtn a*b/gcd(a,b);}
-inline lli bin_pow(lli x,lli y){lli z=1;whl(y){if(y&1)z=mod(z*x);x=mod(sqr(x)),y>>=1;}rtn z;}
-template<typename istream,typename first_type,typename second_type>inline istream& operator>>(istream& cin,pr<first_type,second_type>& x){rtn cin>>x.x>>x.y;}
-template<typename ostream,typename first_type,typename second_type>inline ostream& operator<<(ostream& cout,const pr<first_type,second_type>& x){rtn cout<<x.x<<" "<<x.y;}
-template<typename istream,typename type>inline istream& operator>>(istream& cin,vec<type>& x){rep(i,sz(x))cin>>x[i];rtn cin;}
-template<typename ostream,typename type>inline ostream& operator<<(ostream& cout,const vec<type>& x){rep(i,sz(x))cout<<x[i]<<(i+1==sz(x)?"":" ");rtn cout;}
-inline ostream& pdb(int prcs,db x){rtn cout<<setprecision(prcs)<<fixed<<(sgn(x)?(x):0);}
-template<typename type>inline void bit_inc(vec<type>& st,int x,type inc){whl(x<sz(st))st[x]+=inc,x|=x+1;}
-template<typename type>inline type bit_sum(const vec<type>& st,int x){type s=0;whl(x>=0)s+=st[x],x=(x&(x+1))-1;rtn s;}
-template<typename type>inline type bit_kth(const vec<type>& st,int k){int x=0,y=0,z=0;whl((1<<(++y))<=sz(st));rrep(i,y){if((x+=1<<i)>sz(st)||z+st[x-1]>k)x-=1<<i;else z+=st[x-1];}rtn x;}
-inline void make_set(vi& st){rep(i,sz(st))st[i]=i;}
-inline int find_set(vi& st,int x){int y=x,z;whl(y!=st[y])y=st[y];whl(x!=st[x])z=st[x],st[x]=y,x=z;rtn y;}
-inline bool union_set(vi& st,int a,int b){a=find_set(st,a),b=find_set(st,b);rtn a!=b?st[a]=b,true:false;}
-inline void make_set(vpii& st){rep(i,sz(st))st[i]=mp(i,1);}
-inline int find_set(vpii& st,int x){int y=x,z;whl(y!=st[y].x)y=st[y].x;whl(x!=st[x].x)z=st[x].x,st[x].x=y,x=z;rtn y;}
-inline bool union_set(vpii& st,int a,int b){a=find_set(st,a),b=find_set(st,b);rtn a!=b?(st[a].y>st[b].y?st[a].x=b,st[a].y+=st[b].y:st[b].x=a,st[b].y+=st[a].y),true:false;}
-template<typename type>inline void merge(type& a,type& b){if(sz(a)<sz(b))swap(a,b);whl(sz(b))a.ins(*b.begin()),b.ers(b.begin());}
-
-//初始化;
-struct Initializer{
-#ifndef DEBUG
-Initializer(){ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);}
-#else
-~Initializer(){runtime();}
-#endif
-}initializer;
-
-//非标准;
-#define for_each(e,s) for (__typeof__((s).begin()) e=(s).begin();e!=(s).end();++e)
-#include <ext/rope>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <ext/pb_ds/tag_and_trait.hpp>
-typedef __gnu_cxx::rope<char> rope;
-template<typename key,typename value>class ext_map:public __gnu_pbds::tree<key,value,less<key>,__gnu_pbds::rb_tree_tag,__gnu_pbds::tree_order_statistics_node_update>{};
-#define ctz __builtin_ctz
-#define clz __builtin_clz
-#define bc __builtin_popcount
-#else
-//上面部分的代码生成的Precompiled Headers
-#include <Core>
-#endif
-
+const int N=120,M=102000;
+const int inf=0x3f3f3f3f;
+int head[N],v[M],c[M],nxt[M],t;
+int q[N],dis[N];
+int S,T;
+bool vis[20000][N];
+struct E
+{
+	int a,b;
+}e[M];
+int top=0;
+int n,m,K;
+int maxflow[N][N];
+bool g[N][N];
+int fa[N];
+int gf(int x)
+{
+	if(fa[x]!=x)fa[x]=gf(fa[x]);
+	return fa[x];
+}
+inline void add(int a,int b,int cc)
+{
+	nxt[++t]=head[a];v[t]=b;c[t]=cc;head[a]=t;
+	nxt[++t]=head[b];v[t]=a;c[t]=cc;head[b]=t;
+}
+bool bfs()
+{
+	static int h,tail,s,i;
+	memset(dis,-1,sizeof(dis));
+	dis[q[h=tail=1]=S]=0;
+	while(h<=tail)
+	{
+		s=q[h++];
+		for(i=head[s];i;i=nxt[i])
+			if(dis[v[i]]==-1&&c[i])
+			{
+				dis[v[i]]=dis[s]+1;
+				if(v[i]==T)return true;
+				q[++tail]=v[i];
+			}
+	}
+	return false;
+}
+int aug(int x,int sum)
+{
+	if(x==T)return sum;
+	int i,os=sum,a;
+	for(i=head[x];i&&sum;i=nxt[i])
+	if(dis[v[i]]==dis[x]+1&&c[i])
+	{
+		a=aug(v[i],min(sum,c[i]));
+		sum-=a;c[i]-=a;c[i^1]+=a;
+	}
+	if(os==sum)dis[x]=-1;
+	return os-sum;
+}
+void init()
+{
+	int i,j;
+	memset(head,0,sizeof(head));t=1;
+	for(i=1;i<=n;i++)
+		for(j=1;j<i;j++)
+			if(g[i][j])add(i,j,1);
+}
+void dfs(int x,int p)
+{
+	int i;
+	vis[p][x]=true;
+	for(i=head[x];i;i=nxt[i])if(c[i]&&!vis[p][v[i]])dfs(v[i],p);
+}
+void work(int x)
+{
+	int x1,x2,i,j,flow=0;
+	S=T=0;
+	for(i=1;i<=n;i++)
+		if(vis[x][i])
+		{
+			if(!S)S=i;
+			else if(!T)T=i;
+		}
+	if(!T)return ;
+	init();
+	while(bfs())flow+=aug(S,inf);
+	x1=++top;
+	x2=++top;
+	memset(vis[x1],false,sizeof(vis[x1]));
+	memset(vis[x2],false,sizeof(vis[x2]));
+	dfs(S,x1);
+	for(i=1;i<=n;i++)if(!vis[x1][i])vis[x2][i]=true;
+	for(i=1;i<=n;i++)if(vis[x1][i])
+		for(j=1;j<=n;j++)if(vis[x2][j])
+			maxflow[i][j]=maxflow[j][i]=min(maxflow[i][j],flow);
+	for(i=1;i<=n;i++)vis[x1][i]&=vis[x][i];
+	for(i=1;i<=n;i++)vis[x2][i]&=vis[x][i];
+	work(x1);
+	work(x2);
+}
 int main()
 {
+	int i,j;
+	bool flag;
+	while(~scanf("%d%d%d",&n,&m,&K))
+	{
+		memset(g,false,sizeof(g));
+		for(i=1;i<=m;i++)
+			scanf("%d%d",&e[i].a,&e[i].b),g[e[i].a][e[i].b]=g[e[i].b][e[i].a]=true;
+		flag=true;
+		while(flag)
+		{
+			top=1;
+			flag=false;
+			memset(maxflow,inf,sizeof(maxflow));
+			top=1;
+			memset(vis[top],false,sizeof(vis[top]));
+			for(i=1;i<=n;i++)
+				vis[top][i]=true;
+			work(top);
+			for(i=1;i<=n;i++)
+				for(j=1;j<=n;j++)
+					if(maxflow[i][j]<K&&g[i][j])
+						flag=true,g[i][j]=g[j][i]=false;
+		}
+		for(i=1;i<=n;i++)
+			fa[i]=i;
+//		for(i=1;i<=n;i++,puts(""))
+//			for(j=1;j<=n;j++)
+//				printf("%d ",maxflow[i][j]);
+		for(i=1;i<=n;i++)
+			for(j=1;j<=n;j++)
+				if(g[i][j])
+					fa[gf(i)]=gf(j);
+		for(i=1;i<=n;i++)
+			fa[i]=gf(i);
+		sort(fa+1,fa+1+n);
+		int ans=0;
+		for(i=1;i<=n;i++)
+			if(fa[i]!=fa[i-1])ans++;
+		printf("%d\n",ans);
+	}
 }
+//const int MAXN=14;
+//int dp[MAXN][MAXN];
+//
+//int main()
+//{
+////	int ans = 0;
+////	for (int i = 0; i <= 10; i++)
+////		for (int j = i; j <= 10; j++)
+////			for (int k = j; k <= 10; k++)
+////				for (int l = k; l <= 10; l++)
+////					for (int m = l; m <= 10; m++)
+////						for (int n = m; n <= 10; n++)
+////							for (int o = n; o <= 10; o++)
+////								for (int p = o; p <= 10; p++)
+////									if (i + j + k + l + m + n + o + p == 8)
+////										ans++;
+////	cout << ans << endl;
+//	for (int i=0;i<MAXN;i++) dp[0][i]=1;
+//	for (int i=1;i<MAXN;i++)
+//		for (int j=1;j<MAXN;j++)
+//			dp[i][j]=dp[i][j-1]+(i-j>=0?dp[i-j][j]:0);
+//	for (int i=0;i<MAXN;i++)
+//	{
+//		for (int j=1;j<=i;j++)
+//			cout<<dp[i][j]-dp[i][j-1]<<" ";
+//		cout<<endl;
+//	}
+//	for (int i=1;i<MAXN;i++)
+//	{
+//		cout<<dp[i][i]<<endl;
+//	}
+//}
--- java-workspace/src/Main.java
+++ java-workspace/src/Main.java
@@ -1,48 +1,78 @@
-import java.io.BufferedInputStream;
-import java.math.BigInteger;
-import java.util.Formatter.BigDecimalLayoutForm;
-import java.util.Scanner;
+import java.awt.Point;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(new BufferedInputStream(System.in));
-		int TC = in.nextInt();
-		for (int tc = 1; tc <= TC; tc++) {
-			System.out.print("Case " + tc + ": ");
-			int n = in.nextInt();
-			BigInteger a = in.nextBigInteger().subtract(BigInteger.ONE);
-			BigInteger x = in.nextBigInteger().subtract(BigInteger.ONE);
-			BigInteger b = in.nextBigInteger().subtract(BigInteger.ONE);
-			BigInteger y = in.nextBigInteger().subtract(BigInteger.ONE);
-			BigInteger s = a.xor(b), t = x.xor(y);
-			char[] S = new char[n * 2], T = new char[n];
-			for (int i = 0; i < n; i++) {
-				S[i] = S[i + n] = s.testBit(i) ? '1' : '0';
-				T[i] = t.testBit(i) ? '1' : '0';
-			}
-			boolean ans = false;
-			int[] pi = new int[n];
-			pi[0] = -1;
-			for (int i = 1, j = -1; i < n; i++) {
-				while (j != -1 && T[j + 1] != T[i])
-					j = pi[j];
-				if (T[j + 1] == T[i])
-					j++;
-				pi[i] = (j != -1 && i + 1 < n && T[j + 1] == T[i + 1]) ? pi[j]
-						: j;
-			}
-			for (int i = 0, j = -1; i < n * 2; i++) {
-				while (j != -1 && T[j + 1] != S[i])
-					j = pi[j];
-				if (T[j + 1] == S[i])
-					j++;
-				if (j == n - 1) {
-					ans = true;
-					j = pi[j];
-				}
-			}
-			System.out.println(ans ? "Yes" : "No");
+	public static class Scanner {
+
+		private InputStream in;
+
+		public Scanner(InputStream in) {
+			this.in = in;
 		}
+
+		public int nextInt() {
+			try {
+				int c = in.read(), flag, x;
+				while (!Character.isDigit(c) && c != '-')
+					c = in.read();
+				if (c == '-') {
+					flag = -1;
+					x = 0;
+				} else {
+					flag = 1;
+					x = c - '0';
+				}
+				c = in.read();
+				while (Character.isDigit(c)) {
+					x = x * 10 + c - '0';
+					c = in.read();
+				}
+				return flag * x;
+			} catch (IOException e) {
+				return 0;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+		int n = in.nextInt(), m = in.nextInt(), k = in.nextInt();
+		int[] a = new int[k];
+		for (int i = 0; i < k; i++)
+			a[i] = in.nextInt();
+		if (k == n || m > (n - 3) * (n - 2) / 2 + (n - 1))
+			out.println(-1);
+		else {
+			int core = 0;
+			for (int i = 1; i <= n; i++) {
+				boolean found = false;
+				for (int j = 0; j < k; j++)
+					if (a[j] == i)
+						found = true;
+				if (!found)
+					core = i;
+			}
+			int left = 0, right = 0;
+			for (int i = n; i >= 1; i--)
+				if (i != core)
+					left = i;
+			for (int i = 1; i <= n; i++)
+				if (i != core)
+					right = i;
+			int cnt = 0;
+			for (int i = 1; i <= n && cnt < m - 1; i++)
+				for (int j = i + 1; j <= n && cnt < m - 1; j++)
+					if (i != left && j != left) {
+						out.println(i + " " + j);
+						cnt++;
+					}
+			out.println(left + " " + core);
+		}
+		out.close();
 	}
 }
